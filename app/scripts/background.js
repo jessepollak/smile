73a3905dc8486cb25e1bbcd3856fdea7b7db81aa
Jesse Pollak <jpollak92@gmail.com>
2013-11-03 22:17:10 -0800

    smile v0.0.1

---
+++ app/scripts/background.js
@@ -0,0 +1,28 @@
+'use strict';
+
+chrome.runtime.onInstalled.addListener(function (details) {
+    console.log('previousVersion', details.previousVersion);
+});
+
+(function() {
+
+
+    (function() {
+
+
+        function Smile() {
+            chrome.tabs.onUpdated.addListener(this.tabUpdated);
+        }
+
+        Smile.prototype.tabUpdated = function (tabID, changeInfo, tab) {
+            if (tab.url && tab.url.match("www.amazon.com")) {
+                chrome.tabs.update(tabID, {
+                    url: tab.url.replace('www.amazon.com', 'smile.amazon.com')
+                })
+            }
+        };
+
+        new Smile();
+    })();
+
+}).call(this);
